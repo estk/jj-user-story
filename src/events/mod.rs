3a3cdc97b096a402851981fe648f659162dd9ba8
Evan Simmons <esims89@gmail.com>
2024-03-10 15:52:39 -0700

    EventDispatcher single dispatch point

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,3 +1,5 @@
+use anyhow::anyhow;
+use std::time::Duration;
 use tokio::select;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
@@ -5,9 +7,10 @@ use tokio::sync::mpsc::error::SendError;
 use tokio::task::JoinHandle;
 
 mod config;
-pub use config::Config;
-
 mod event;
+mod test;
+
+pub use config::Config;
 pub use event::Event;
 
 pub struct EventDispatcher {
@@ -20,6 +23,9 @@ const IN_BUF_SIZE: usize = 10;
 const OUT_BUF_SIZE: usize = 10;
 
 impl EventDispatcher {
+    pub fn try_recv(&mut self) -> Result<String, broadcast::error::TryRecvError> {
+        self.out_rx.try_recv()
+    }
     pub fn subscribe(&self) -> broadcast::Receiver<String> {
         self.out_rx.resubscribe()
     }
@@ -30,7 +36,7 @@ impl EventDispatcher {
         let (in_tx, in_rx) = mpsc::channel(IN_BUF_SIZE);
         let (out_tx, out_rx) = broadcast::channel(OUT_BUF_SIZE);
 
-        let worker = Self::spawn(c.round_floats, in_rx, out_tx);
+        let worker = Self::spawn(c.replay_interval, c.round_floats, in_rx, out_tx);
         Self {
             out_rx,
             in_tx,
@@ -38,11 +44,15 @@ impl EventDispatcher {
         }
     }
     fn spawn(
+        replay_interval: Duration,
         round_floats: bool,
         mut in_rx: mpsc::Receiver<Event>,
         out_tx: broadcast::Sender<String>,
-    ) -> JoinHandle<()> {
+    ) -> JoinHandle<anyhow::Result<()>> {
         tokio::spawn(async move {
+            let mut last_float_event = None;
+            let mut interval = tokio::time::interval(replay_interval);
+
             loop {
                 select! {
                     Some(e) = in_rx.recv() => {
@@ -50,17 +60,29 @@ impl EventDispatcher {
                             Event::String(s) => s,
                             Event::Float(f) => {
                                 let f = if round_floats {f.round()} else {f};
-                                format!("Float: {}", f)
+
+                                let m = format!("Float: {}", f);
+                                last_float_event.replace(m.clone());
+                                m
                             },
                         };
-                        eprintln!("{msg}");
-                        if out_tx.send(msg).is_err() {
-                            // no listeners
-                            return
+                        dispatch(&msg, &mut out_tx)?;
+                    },
+                    _ = interval.tick() => {
+                        if let Some(msg) = &last_float_event {
+                            dispatch(&msg, &mut out_tx)?;
                         }
                     }
                 }
             }
         })
     }
+}
+
+fn dispatch(msg: &str, out_tx: &mut broadcast::Sender<String>) -> anyhow::Result<()> {
+    eprintln!("{msg}");
+    if out_tx.send(msg.to_string()).is_err() {
+        anyhow!("No listeners");
+    }
+    Ok(())
 }
